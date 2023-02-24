e5e3286e608c54ca202ba476cbdbb33c4ee5929b
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2023-02-24 15:15:09 +0100

    fixes and updated implementation for API to return beneficiary list

--- src/main/java/com/example/money_way/enums/TransactionType.java
+++ src/main/java/com/example/money_way/enums/TransactionType.java
@@ -1,5 +1,5 @@
 package com.example.money_way.enums;
 
 public enum TransactionType {
-    LOCAL, BANK, BILL, AIRTIME
+    LocalTransfer, ThirdPartyTransfer, BILL, AIRTIME
 }
--- src/main/java/com/example/money_way/service/impl/BeneficiaryServiceImpl.java
+++ src/main/java/com/example/money_way/service/impl/BeneficiaryServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.money_way.service.impl;
 
 import com.example.money_way.dto.response.ApiResponse;
-import com.example.money_way.enums.TransactionType;
 import com.example.money_way.model.Beneficiary;
 import com.example.money_way.model.User;
 import com.example.money_way.repository.BeneficiaryRepository;
@@ -10,9 +9,7 @@ import com.example.money_way.utils.AppUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -24,41 +21,13 @@ public class BeneficiaryServiceImpl implements BeneficiaryService {
     public ApiResponse<List<Beneficiary>> getBeneficiaries(String transactionType) {
         User user = appUtil.getLoggedInUser();
 
-        List<Beneficiary> beneficiaryList = beneficiaryRepository.findAllByUserId(user.getId());
-
-        List<Beneficiary> response = new ArrayList<>();
-
-        if (transactionType.equalsIgnoreCase("TRANSFER")){
-
-            response = beneficiaryList.stream()
-                    .filter(beneficiary ->
-                            beneficiary.getTransactionType().equals(TransactionType.BANK) ||
-                            beneficiary.getTransactionType().equals(TransactionType.LOCAL)
-                            )
-                    .collect(Collectors.toList());
-
-        } else if (transactionType.equalsIgnoreCase("AIRTIME")){
-
-            response = beneficiaryList.stream()
-                    .filter(beneficiary ->
-                            beneficiary.getTransactionType().equals(TransactionType.AIRTIME)
-                    )
-                    .collect(Collectors.toList());
-
-        } else if (transactionType.equalsIgnoreCase("BILL")){
-
-            response = beneficiaryList.stream()
-                    .filter(beneficiary ->
-                            beneficiary.getTransactionType().equals(TransactionType.BILL)
-                    )
-                    .collect(Collectors.toList());
-
-        }
+        List<Beneficiary> beneficiaryList = beneficiaryRepository
+                .findAllByUserIdAndTransactionType(user.getId(), transactionType);
 
         ApiResponse<List<Beneficiary>> apiResponse = new ApiResponse<>();
         apiResponse.setStatus("SUCCESS");
         apiResponse.setMessage("Successfully Fetched Beneficiaries of User");
-        apiResponse.setData(response);
+        apiResponse.setData(beneficiaryList);
 
         return apiResponse;
     }
