bf25135d6c6297ab875c4251e701047156a49098
Janet Kalu <janetkalu82@gmail.com>
2023-03-04 00:32:15 +0100

    Refactored codes

--- src/main/java/com/example/money_way/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/money_way/exception/GlobalExceptionHandler.java
@@ -82,5 +82,13 @@ public class GlobalExceptionHandler {
         return  new ApiResponse<>("Failed", "Error: " +ex.getMessage(),null);
 
     }
+    @ExceptionHandler(InsufficientFundsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ApiResponse<String> handleInsufficientFundsException(InsufficientFundsException ex){
+        logger.error(ex.getMessage());
+        return  new ApiResponse<>("Failed", "Error: " +ex.getMessage(),null);
+
+    }
 
 }
